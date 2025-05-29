535ce0f9ea7f25a6b3929510a138d842297a208e
ItaloBarc <italobarc01@gmail.com>
2025-05-29 11:27:27 -0300

    atualização de erro com string

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,51 +1,62 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    // Variáveis para guardar as informações das cartas
+    //informações das cartas
     char estado1, estado2;
     char codigo1[4], codigo2[4];
     char nomeCidade1[50], nomeCidade2[50];
     int populacao1, populacao2;
     float area1, area2, pib1, pib2;
     int pontosTuristicos1, pontosTuristicos2;
-    
-    // Pedir ao usuário para inserir os dados da primeira carta
+
+    // Cadastro da primeira carta
     printf("\n>>> Cadastro da primeira carta <<<\n");
     printf("Estado (A-H): ");
     scanf(" %c", &estado1);
+    getchar();
+
     printf("Código da Carta (ex: A01): ");
     scanf(" %s", codigo1);
+    getchar();
+
     printf("Nome da Cidade: ");
-    scanf(" %s", nomeCidade1);
+    fgets(nomeCidade1, sizeof(nomeCidade1), stdin);
+    nomeCidade1[strcspn(nomeCidade1, "\n")] = '\0';
+
     printf("População: ");
-    scanf(" %d", &populacao1);
+    scanf("%d", &populacao1);
     printf("Área (em km²): ");
-    scanf(" %f", &area1);
+    scanf("%f", &area1);
     printf("PIB (em bilhões de reais): ");
-    scanf(" %f", &pib1);
+    scanf("%f", &pib1);
     printf("Número de Pontos Turísticos: ");
-    scanf(" %d", &pontosTuristicos1);
-    
-   
-    
-    // Pedir ao usuário para inserir os dados da segunda carta
+    scanf("%d", &pontosTuristicos1);
+
+    // Cadastro da segunda carta
     printf("\n>>> Cadastro da segunda carta <<<\n");
     printf("Estado (A-H): ");
     scanf(" %c", &estado2);
+    getchar(); 
+
     printf("Código da Carta (ex: B02): ");
     scanf(" %s", codigo2);
+    getchar(); 
+
     printf("Nome da Cidade: ");
-    scanf(" %s", nomeCidade2);
+    fgets(nomeCidade2, sizeof(nomeCidade2), stdin);
+    nomeCidade2[strcspn(nomeCidade2, "\n")] = '\0'; 
+
     printf("População: ");
-    scanf(" %d", &populacao2);
+    scanf("%d", &populacao2);
     printf("Área (em km²): ");
-    scanf(" %f", &area2);
+    scanf("%f", &area2);
     printf("PIB (em bilhões de reais): ");
-    scanf(" %f", &pib2);
+    scanf("%f", &pib2);
     printf("Número de Pontos Turísticos: ");
-    scanf(" %d", &pontosTuristicos2);
-    
-   // Exibir as informações da primeira carta
+    scanf("%d", &pontosTuristicos2);
+
+    // Exibir as informações da primeira carta
     printf("\n>>> Informações da primeira carta <<<\n");
     printf("Estado: %c\n", estado1);
     printf("Código: %s\n", codigo1);
@@ -55,7 +66,7 @@ int main() {
     printf("PIB: %.2f bilhões de reais\n", pib1);
     printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
 
-  // Exibir as informações da segunda carta
+    // Exibir as informações da segunda carta
     printf("\n>>> Informações da segunda carta <<<\n");
     printf("Estado: %c\n", estado2);
     printf("Código: %s\n", codigo2);
@@ -65,6 +76,5 @@ int main() {
     printf("PIB: %.2f bilhões de reais\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
 
-  
     return 0;
 }
